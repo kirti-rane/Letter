c2f818818efd1c5f3e38ff2912b070a608ceb130
kirti-rane <122624081+kirti-rane@users.noreply.github.com>
2023-01-28 20:56:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post("/failure", function(req, res){
     res.redirect("/");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 
     console.log("port started at 3000");
 });
